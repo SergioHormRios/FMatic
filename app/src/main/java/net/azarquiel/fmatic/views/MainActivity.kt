63532ee333f8bc00b37b263eca6257f54d79c28b
SergioHormigos <alumno.215424@ies-azarquiel.es>
2023-05-13 20:42:52 +0200

    estructura drawer

---
+++ app/src/main/java/net/azarquiel/fmatic/views/MainActivity.kt
@@ -0,0 +1,87 @@
+package net.azarquiel.fmatic.views
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import net.azarquiel.fmatic.R
+import net.azarquiel.fmatic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        drawerLayout = binding.drawerLayout
+
+        navView = binding.navView
+        navView.setNavigationItemSelectedListener(this)
+
+        //Genera el icono de las tres rayas que abre el cajon
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, binding.appBarMain.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        var fragment:Fragment? = null
+        when (item.itemId) {
+            R.id.nav_pilotos -> {
+            }
+            R.id.nav_circuitos -> {
+            }
+            R.id.nav_temporadas -> {
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    /*
+        Cuando pulsemos el botón de regreso mientras tenemos el
+        drawerLayout desplegado, se cerrara en vez de salir de
+        la Activity
+     */
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+}
